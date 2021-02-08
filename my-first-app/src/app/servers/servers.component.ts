ea49ee5532af52ed56e20203b38140f9506d1520
tizenegy <bastian.elfert@outlook.com>
2021-02-08 19:30:21 +0100

    added button and some logic to demonstrate string interpolation and property binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -6,8 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  //this property is an example of property binding, 
+  //as it is bound to the disabled property of the button in the html file.
+  allowNewServer: boolean = false;
 
-  constructor() { }
+  constructor() { 
+    //this timeout function is used to demonstrate the property binding via allowNewServer
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
