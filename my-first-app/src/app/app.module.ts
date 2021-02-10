db0049aabc9f840827985926df0fd5b6c87d2848
tizenegy <bastian.elfert@outlook.com>
2021-02-10 16:32:00 +0100

    added example for event binding and added import for FormsModule

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './alerts/warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './alerts/success-alert/success-alert.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SuccessAlertComponent } from './alerts/success-alert/success-alert.comp
     SuccessAlertComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   //as it is bound to the disabled property of the button in servers.component.html.
   allowNewServer: boolean = false;
   serverCreationStatus = "No server was created.";
+  serverName = "";
 
   constructor() { 
     //this timeout function is used to demonstrate the property binding via allowNewServer
@@ -26,4 +27,9 @@ export class ServersComponent implements OnInit {
     this.serverCreationStatus = "Server was created.";
   }
 
+  //example of event binding: taking the value of an input and displaying it below.
+  //passing with $event und explicit casting <>
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
